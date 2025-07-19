ceee00152fc18dd8cffb18ab42e3ad3307487d76
JunsangLee <162899822+Junsang-L33@users.noreply.github.com>
2025-07-20 00:28:11 +0900

    Update softmax.py

--- softmax.py
+++ softmax.py
@@ -13,8 +13,20 @@ y = exp_a / sum_exp_a
 print(y)
 """
 
+""" overflow problem
+a = np.array([1010, 1000, 990])
+
+np.exp(a) / np.sum(np.exp(a))
+
+C = np.max(a)
+a - C
+
+np.exp(a - C) / np.sum(np.exp(a - C))
+"""
+
 def softmax(a):
-  exp_a = np.exp(a)
+  c = np.max(a)
+  exp_a = np.exp(a - c) #overflow countermeasures
   sum_exp_a = np.sum(exp_a)
   y = exp_a / sum_exp_a
 
