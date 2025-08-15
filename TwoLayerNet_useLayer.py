b155b311f31f7a0781cca5042ad5809451164e3e
JunsangLee <162899822+Junsang-L33@users.noreply.github.com>
2025-08-15 22:37:06 +0900

    Update TwoLayerNet_useLayer.py

--- TwoLayerNet_useLayer.py
+++ TwoLayerNet_useLayer.py
@@ -39,6 +39,43 @@ class Affine:
 
     return dx
 
+def softmax(x):
+    if x.ndim == 2:
+        x = x - np.max(x, axis=1, keepdims=True)
+        exp = np.exp(x)
+        return exp / np.sum(exp, axis=1, keepdims=True)
+    x = x - np.max(x)
+    exp = np.exp(x)
+    return exp / np.sum(exp)
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        y = y.reshape(1, -1)
+        t = t.reshape(1, -1)
+    if y.size == t.size:  
+        t = t.argmax(axis=1)
+    batch = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch), t] + 1e-7)) / batch
+
+class SoftmaxWithLoss:
+  def __init__(self):
+    self.loss = None
+    self.y = None
+    self.t = None
+
+  def forward(self, x, t):
+    self.t = t
+    self.y = softmax(x)
+    self.loss = cross_entropy_error(self.y, self.t)
+    return self.loss
+
+  def backward(self, dout = 1):
+    batch_size = self.t.shape[0]
+    dx = (self.y - self.t) / batch_size
+
+    return dx
+
+
 def numerical_gradient(f, x):
     h = 1e-4 
     grad = np.zeros_like(x)
